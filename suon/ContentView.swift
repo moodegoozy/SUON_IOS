aa410f62ed6ab1b7c8ba994efdc2156ccda29ae8
ahmed <moode434@gmail.com>
2026-05-16 01:17:23 +0200

    Initial Commit

---
+++ suon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  suon
+//
+//  Created by user289721 on 5/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ suon/suonApp.swift
@@ -0,0 +1,17 @@
+//
+//  suonApp.swift
+//  suon
+//
+//  Created by user289721 on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct suonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
